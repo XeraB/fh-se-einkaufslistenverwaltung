11093d619ac13206481ed23594155747c916881a
Janet B <jb498734@fh-muenster.de>
2022-03-21 18:17:45 +0100

    Add IntelliJ Project

---
+++ src/main/java/de/fhms/sweng/einkaufslistenverwaltung/EinkaufslistenverwaltungApplication.java
@@ -0,0 +1,13 @@
+package de.fhms.sweng.einkaufslistenverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufslistenverwaltungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EinkaufslistenverwaltungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fhms/sweng/einkaufslistenverwaltung/EinkaufslistenverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhms.sweng.einkaufslistenverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinkaufslistenverwaltungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
